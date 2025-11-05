0d58a6dab0f0842d4a46614843c2f4fb295b29d5
mfabri31 <fabriciomedina9531@gmail.com>
2025-11-05 11:04:00 -0300

    chore: :heavy_plus_sign: install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1>React Projects</h1>
+      <h1 className="text-3xl font-bold text-center py-10">React Projects</h1>
     </>
   );
 }
